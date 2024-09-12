a72f259bc25270c65b4ea93a0ba39fae231c8421
Ayush Soni <145641544+Ayssoni@users.noreply.github.com>
2024-09-13 02:27:29 +0530

    Add files via upload

---
+++ code.java
@@ -0,0 +1,147 @@
+// class Student{
+//    String name;
+//    int id;
+//    public void student_info(){
+//     System.out.println(this.name);
+//     System.out.println(this.id);
+
+//    }
+
+// }
+// public class code{
+//     public static void main(String[] args) {
+//         Student st =new Student();
+//         st.name="Rohan";
+//         st.id=108;
+
+//         st.student_info();
+
+//     }
+// }
+// class Student{
+//     String name;
+//     int id;
+//     public void student_info(String name,int id){
+//         System.out.println(this.name);
+//         System.out.println(this.id);
+//     }
+//     Student(String name,int id){
+//     this.name=name;
+//     this.id=id;
+   
+ 
+//     }
+//     }
+ /*public class code{
+    public static void main(String[] args) {
+        
+        
+        try {
+           int a = 12/0;
+
+        } catch (Exception e) {
+            System.out.println(e);
+            int a=0;
+        }
+        finally{
+            System.out.println("The Flow of the Program Should Be normal");
+        }
+    }
+}
+ */
+ 
+ /*class Bgmi{
+    String bot;
+    String pro;
+    String goat;
+    public void Bgmi_players( String bot[],String pro[],String goat[]){
+        System.out.println(this.bot);
+        System.out.println(this.pro);
+        System.out.println(this.goat);
+    }
+    
+    
+ }
+ class code{
+    public static void main(String[] args) {
+        Bgmi bg = new Bgmi();
+       bg.bot = "Moit";
+       bg.pro = "sohit";
+       bg.goat = "loat";
+       bg.Bgmi_players(null, null, null);
+    }
+ }
+ 
+*/
+
+/*oops == "polymorphism == Single Task in Multiple Ways." 
+ "Encapsulation == Binding Or Wraping Code and Data together into a single unit"
+ "Abstraction == Hiding internal details and showing functionalities"
+ "Inheritance == A Mechanism where one class aquire the properties and behaviours of parent class"
+
+*/
+
+
+
+/*class code{
+    public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter username");
+
+    String userName = myObj.nextLine();  // Read user input
+    System.out.println("Username is: " + userName);  // Output user input
+  }
+}*/
+
+/*class code {
+    public static void main(String[] args) {
+      int x, y, sum;
+      Scanner myObj = new Scanner(System.in); // Create a Scanner object
+      System.out.println("Type a number:");
+      x = myObj.nextInt(); // Read user input
+  
+      System.out.println("Type another number:");
+      y = myObj.nextInt(); // Read user input
+  
+      sum = x + y;  // Calculate the sum of x + y
+      System.out.println("Sum is: " + sum); // Print the sum
+    }
+  } 
+  
+*/
+// class Employee{  
+//     float salary=40000;  
+//    }  
+/*class funtion_Sort{
+
+  public void function_sort(){
+    int[] a=new int[12];
+
+
+  }
+}
+
+
+*/
+
+
+
+   class code{  
+    public static void main(String args[]){  
+     /*  byte b;int i = 423;double d = 720.16;
+      // b=(byte)i;
+      // System.out.println(+b);
+      i = (int)d;
+      System.out.println(+i);*/
+      for(int i=1;i<=5;i++){
+        
+        for(int j=1;j<=i;j++){
+          System.out.println("*");
+          
+        }
+        System.out.println("\n");
+      }
+      
+      
+   } 
+   }  
